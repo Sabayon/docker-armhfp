b6770a83d7233f222a051746c8cb76f86f5b2aa6
mudler <mudler@sabayon.org>
2016-01-07 23:11:40 +0100

    first commit

---
+++ odroid-x2-u-u2/scripts/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/usr/sbin/env-update
+. /etc/profile
+
+setup_bootfs_fstab() {
+	# add /dev/mmcblk0p1 to /etc/fstab
+	local boot_part_type="${1}"
+	echo "/dev/mmcblk0p1  /boot  ${boot_part_type}  defaults  0 2" >> /etc/fstab
+}
+
+setup_rootfs_fstab() {
+	echo "/dev/mmcblk0p2 / ext4 noatime 0 1" >> /etc/fstab
+}
+
+rm -rfv /etc/fstab
+#setup_bootfs_fstab "vfat"
+setup_rootfs_fstab
+
+exit 0
