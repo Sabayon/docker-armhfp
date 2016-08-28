3d6f8fdcca6cf2489c3a0549def74ca6ea8fb32d
Geaaru <geaaru@gmail.com>
2016-08-29 00:11:55 +0200

    [bananapi] Add bananapi script and kernel v4.2.0 tarballs

---
+++ bananapi/scripts/setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Description: Script for prepare docker image of bananapi tree
+#              from sabayon/armhfp tree.
+
+/usr/sbin/env-update
+. /etc/profile
+
+setup_bootfs_fstab() {
+	# add /dev/mmcblk0p1 to /etc/fstab
+	local boot_part_type="${1}"
+	echo "/dev/mmcblk0p1  /boot  ${boot_part_type}  defaults  0 2" >> /etc/fstab
+}
+
+setup_rootfs_fstab() {
+	echo "/dev/mmcblk0p2 / ext4 noatime 0 1" >> /etc/fstab
+}
+
+# Force armv7l entropy architecture (kernel is 64bit)
+echo "armv7l" > /etc/entropy/.arch
+echo "~arm" > /etc/entropy/packages/package.keywords
+echo "arm" >> /etc/entropy/packages/package.keywords
+
+# Perform package upgrades
+ACCEPT_LICENSE=* equo up && equo u
+
+# Networkmanager gives issues on aarch64
+systemctl disable NetworkManager
+equo rm net-misc/networkmanager
+cat > /etc/systemd/network/default_dhcp.network << "EOF"
+[Network]
+DHCP=ipv4
+
+[DHCP]
+UseDomains=true
+EOF
+
+systemctl enable systemd-networkd
+
+# Cleanup
+equo cleanup
+
+# Accepts configuration updates
+echo -5 | equo conf update
+
+rm -rfv /etc/fstab
+setup_rootfs_fstab
+
+exit 0
