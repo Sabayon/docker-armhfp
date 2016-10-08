174bdb482cd1d2ae3cb4853961cd53b6026a4127
mudler <mudler@sabayon.org>
2016-10-08 19:18:50 +0200

    rpi-openrc: add experimental flavor

---
+++ rpi-openrc/scripts/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+/usr/sbin/env-update
+. /etc/profile
+export ACCEPT_LICENSE=*
+UPGRADE_REPO="sabayon-limbo"
+
+sed -i 's/multifetch = 3/#multifetch = 3/' /etc/entropy/client.conf
+if [ -n "${UPGRADE_REPO}" ]; then
+	echo "Upgrading system by enabling ${UPGRADE_REPO}"
+	equo repo enable "${UPGRADE_REPO}"
+	FORCE_EAPI=2 equo update
+
+	/usr/bin/equo repo mirrorsort "${UPGRADE_REPO}"  # ignore errors
+	/usr/bin/equo repo mirrorsort sabayonlinux.org
+
+	ETP_NONINTERACTIVE=1 equo upgrade --fetch
+	ETP_NONINTERACTIVE=1 equo upgrade --purge
+	echo "-5" | equo conf update
+fi
+# Be sure to have this on the image, always.
+/usr/bin/equo i sys-apps/sysvinit sys-apps/openrc
+/usr/bin/equo mask sys-apps/systemd-sysv-utils app-misc/sabayon-version
+
+echo -5 | equo conf update && equo cleanup
+sed -i 's/#multifetch = 3/multifetch = 3/' /etc/entropy/client.conf
+
+
+exit 0
